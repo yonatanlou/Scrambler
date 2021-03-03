22346e5a6ac5b61252fa5ed35f4a0b4dbb96db6c
yonatanlou <yonatanlou@gmail.com>
2021-03-03 16:51:18 +0200

    --

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-#the main runner
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import pickle
+
+#suppuse to be the topics that the scraper should generate
+topics = ["dress", "shoes", "berlin"]
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("http://www.google.com")
+search = driver.find_element_by_name("q")
+search.clear()
+search.send_keys("bee movie script")
+search.submit()
+
+pickle.dump( driver.get_cookies() , open("cookies.pkl","wb"))
+cookies = pickle.load(open("cookies.pkl", "rb"))
+for cookie in cookies:
+    driver.add_cookie(cookie)
+
+
